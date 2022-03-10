8181552313020d19b7fb0a19839672736a8feb0c
nurlan-recebov <nurlanrecebov86@gmail.com>
2022-03-10 20:12:57 +0400

    yeni task

---
+++ ProductFeedbackUIResponsive/css/javascript.js
@@ -0,0 +1,24 @@
+function nurlan() {
+    document.getElementById("bir").style.display = "block";
+    document.getElementById("uc").style.display = "none";
+    document.getElementById("iki").style.display = "none";
+    document.getElementById("one").style.opacity = '100%';
+    document.getElementById("two").style.opacity = '40%';
+}
+
+function cahan() {
+    document.getElementById("iki").style.display = "block";
+    document.getElementById("uc").style.display = "none";
+    document.getElementById("bir").style.display = "none";
+    document.getElementById("two").style.opacity = '100%';
+    document.getElementById("one").style.opacity = '40%';
+}
+
+function girov() {
+    document.getElementById("uc").style.display = "block";
+    document.getElementById("iki").style.display = "none";
+    document.getElementById("bir").style.display = "none";
+    document.getElementById("three").style.opacity = '100%';
+    document.getElementById("one").style.opacity = '40%';
+    document.getElementById("two").style.opacity = '40%';
+}
