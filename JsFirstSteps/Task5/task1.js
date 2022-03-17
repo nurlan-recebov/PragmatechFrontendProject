85588996317fa161e0418acda02b3d0a96e443e2
nurlan-recebov <nurlanrecebov86@gmail.com>
2022-03-18 02:07:37 +0400

    yeni task5

--- JsFirstSteps/Task5/task1.js
+++ JsFirstSteps/Task5/task1.js
@@ -1,2 +1,5 @@
 // verilən kəsirli ədədin(nümumə: 4.234) tam hissəsi ilə kəsirli hissəsini ayrı ayrı ekrana çap edən program yazınlel
-let a = 4.234
+let eded = 4.234;
+let tam = eded.toFixed();
+console.log(tam);
+console.log(eded - tam);
