206c9a11cdb4be5a9cc217ed75310d7769ad47ff
nurlan-recebov <nurlanrecebov86@gmail.com>
2022-04-22 02:48:21 +0400

    tasklarhamisihazir

--- DOMCircles/task4/app.js
+++ DOMCircles/task4/app.js
@@ -1,16 +1,19 @@
-let body = document.querySelector('body');
-let circle = document.createElement('div')
-    // body.style.position = 'relative'
-circle.className = 'box'
-body.appendChild(circle)
-circle.style.width = '50px'
-circle.style.height = '50px'
-circle.style.background = 'blue'
-circle.style.borderRadius = '50%'
-circle.style.position = 'absolute'
-    // circle.style.position = 'absolute'
-circle.style.transition = ' all .5s ease'
-let a = Math.floor(Math.random() * (window.innerWidth - 50));
-circle.style.left = `${a}px`
-let b = Math.floor(Math.random() * (window.innerHeight - 50));
-circle.style.top = `${b}px`
+function randomCirclePlacement() {
+    let body = document.querySelector('body');
+    let circle = document.createElement('div')
+        // body.style.position = 'relative'
+    circle.className = 'box'
+    body.appendChild(circle)
+    circle.style.width = '50px'
+    circle.style.height = '50px'
+    circle.style.background = 'blue'
+    circle.style.borderRadius = '50%'
+    circle.style.position = 'absolute'
+        // circle.style.position = 'absolute'
+    circle.style.transition = ' all .5s ease'
+    let a = Math.floor(Math.random() * (window.innerWidth - 50));
+    circle.style.left = `${a}px`
+    let b = Math.floor(Math.random() * (window.innerHeight - 50));
+    circle.style.top = `${b}px`
+}
+randomCirclePlacement()
